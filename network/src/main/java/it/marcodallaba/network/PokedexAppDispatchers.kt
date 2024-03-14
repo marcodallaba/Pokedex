f642e87efd4c6f0f67f96a06320a79540a934094
Marco Dalla Ba <marco.dallaba@vimar.com>
2024-03-14 16:30:49 +0100

    Added PokeApi and PokedexAppDispatchers

---
+++ network/src/main/java/it/marcodallaba/network/PokedexAppDispatchers.kt
@@ -0,0 +1,12 @@
+package it.marcodallaba.network
+
+import javax.inject.Qualifier
+import kotlin.annotation.AnnotationRetention.RUNTIME
+
+@Qualifier
+@Retention(RUNTIME)
+annotation class Dispatcher(val pokedexAppDispatchers: PokedexAppDispatchers)
+
+enum class PokedexAppDispatchers {
+  IO,
+}
