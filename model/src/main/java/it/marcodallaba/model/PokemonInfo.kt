0ded3f2860cbc741efc4e5cfc8246308af1de04a
Marco Dalla Ba <marco.dallaba@vimar.com>
2024-03-18 10:55:56 +0100

    Completed PokemonInfo model

--- model/src/main/java/it/marcodallaba/model/PokemonInfo.kt
+++ model/src/main/java/it/marcodallaba/model/PokemonInfo.kt
@@ -1,5 +1,16 @@
 package it.marcodallaba.model
 
 data class PokemonInfo(
-    val name: String
+    val id: Int,
+    val name: String,
+    val frontImageDefaultUrl: String,
+    val types: List<String>,
+    val stats: List<Stat>,
+    val height: Int,
+    val weight: Int,
+)
+
+data class Stat(
+    val name: String,
+    val baseStat: Int,
 )
