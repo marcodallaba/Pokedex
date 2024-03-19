b91fefddb051eefbe9511d4f653c51d46b6d5aa9
Marco Dalla Ba <marco.dallaba@vimar.com>
2024-03-19 12:25:28 +0100

    Added PokemonRepository

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -33,10 +34,11 @@ android {
 }
 
 dependencies {
+    implementation(project(":network"))
+    implementation(project(":model"))
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
