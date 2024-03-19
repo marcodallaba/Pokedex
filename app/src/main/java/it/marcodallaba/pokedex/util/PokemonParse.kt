771c0c544d10c2535a23c22023a5efb96b60390c
Marco Dalla Ba <marco.dallaba@vimar.com>
2024-03-19 15:00:08 +0100

    Fixed wildcard import

--- app/src/main/java/it/marcodallaba/pokedex/util/PokemonParse.kt
+++ app/src/main/java/it/marcodallaba/pokedex/util/PokemonParse.kt
@@ -1,7 +1,30 @@
 package it.marcodallaba.pokedex.util
 
 import androidx.compose.ui.graphics.Color
-import it.marcodallaba.pokedex.ui.theme.*
+import it.marcodallaba.pokedex.ui.theme.AtkColor
+import it.marcodallaba.pokedex.ui.theme.DefColor
+import it.marcodallaba.pokedex.ui.theme.HPColor
+import it.marcodallaba.pokedex.ui.theme.SpAtkColor
+import it.marcodallaba.pokedex.ui.theme.SpDefColor
+import it.marcodallaba.pokedex.ui.theme.SpdColor
+import it.marcodallaba.pokedex.ui.theme.TypeBug
+import it.marcodallaba.pokedex.ui.theme.TypeDark
+import it.marcodallaba.pokedex.ui.theme.TypeDragon
+import it.marcodallaba.pokedex.ui.theme.TypeElectric
+import it.marcodallaba.pokedex.ui.theme.TypeFairy
+import it.marcodallaba.pokedex.ui.theme.TypeFighting
+import it.marcodallaba.pokedex.ui.theme.TypeFire
+import it.marcodallaba.pokedex.ui.theme.TypeFlying
+import it.marcodallaba.pokedex.ui.theme.TypeGhost
+import it.marcodallaba.pokedex.ui.theme.TypeGrass
+import it.marcodallaba.pokedex.ui.theme.TypeGround
+import it.marcodallaba.pokedex.ui.theme.TypeIce
+import it.marcodallaba.pokedex.ui.theme.TypeNormal
+import it.marcodallaba.pokedex.ui.theme.TypePoison
+import it.marcodallaba.pokedex.ui.theme.TypePsychic
+import it.marcodallaba.pokedex.ui.theme.TypeRock
+import it.marcodallaba.pokedex.ui.theme.TypeSteel
+import it.marcodallaba.pokedex.ui.theme.TypeWater
 
 fun parseTypeToColor(typeName: String): Color {
     return when (typeName.lowercase()) {
