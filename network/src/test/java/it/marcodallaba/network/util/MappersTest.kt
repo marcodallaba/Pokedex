8edd6301f94a3243c7ff1f384ad73fbe6975f833
Marco Dalla Ba <marco.dallaba@vimar.com>
2024-03-28 14:05:39 +0100

    Implemented "test PokemonInfoResponse toModel" test

--- network/src/test/java/it/marcodallaba/network/util/MappersTest.kt
+++ network/src/test/java/it/marcodallaba/network/util/MappersTest.kt
@@ -1,9 +1,11 @@
 package it.marcodallaba.network.util
 
 import com.google.gson.Gson
+import it.marcodallaba.model.Stat
+import it.marcodallaba.network.dto.PokemonInfoResponse
 import it.marcodallaba.network.dto.PokemonListResponse
-import it.marcodallaba.network.dto.Result
 import it.marcodallaba.network.json.page1Json
+import it.marcodallaba.network.json.pikachuJson
 import org.junit.Assert.assertEquals
 
 import org.junit.Test
@@ -34,5 +36,53 @@ class MappersKtTest {
         assertEquals("charmander", result[3].name)
         assertEquals(charmanderImageUrl, result[3].imageUrl)
         assertEquals("4", result[3].number)
+    }
+
+    @Test
+    fun `test PokemonInfoResponse toModel`() {
+        val gson = Gson()
+        val response = gson.fromJson(pikachuJson, PokemonInfoResponse::class.java)
+
+        // Call the function to be tested
+        val result = response.toModel()
+
+        val frontImageDefaultUrl =
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+
+        assertEquals(result.id, 25)
+        assertEquals(result.name, "pikachu")
+        assertEquals(result.frontImageDefaultUrl, frontImageDefaultUrl)
+        assertEquals(result.types, listOf("electric"))
+        assertEquals(
+            result.stats,
+            listOf(
+                Stat(
+                    "hp",
+                    35,
+                ),
+                Stat(
+                    "attack",
+                    55,
+                ),
+                Stat(
+                    "defense",
+                    40,
+                ),
+                Stat(
+                    "special-attack",
+                    50
+                ),
+                Stat(
+                    "special-defense",
+                    50
+                ),
+                Stat(
+                    "speed",
+                    90
+                )
+            )
+        )
+        assertEquals(result.height, 4)
+        assertEquals(result.weight, 60)
     }
 }
